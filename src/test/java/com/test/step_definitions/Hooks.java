fb094e8dc1087bbb971bf31cea9d8dc801acc7ca
Jay Sharma <70809316+jaysharma8-mca@users.noreply.github.com>
2020-11-27 16:25:37 +0530

    Update Hooks.java

--- src/test/java/com/test/step_definitions/Hooks.java
+++ src/test/java/com/test/step_definitions/Hooks.java
@@ -34,7 +34,8 @@ public class Hooks{
     	
        	DesiredCapabilities capability = DesiredCapabilities.chrome();
 		capability.setCapability(CapabilityType.ACCEPT_SSL_CERTS, true);
-    	System.setProperty("webdriver.chrome.driver", "D:/InstalledApplications/apache-tomcat-9.0.1/webapps/chromedriver.exe");
+    	//System.setProperty("webdriver.chrome.driver", "D:/InstalledApplications/apache-tomcat-9.0.1/webapps/chromedriver.exe");
+	    System.setProperty("webdriver.chrome.driver", "C:\\Libs\\chromedriver.exe");
     	
     	
     	/* Code added externally by pallavi */
